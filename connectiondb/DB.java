fdf1b1a038eb14f5d4c83e611abbbbe7ef8ec8a4
Samnoon <36998653+samnoon1971@users.noreply.github.com>
2020-01-16 21:25:21 +0600

    Add files via upload
    commit by  S M Samnoon Abrar

---
+++ connectiondb/DB.java
@@ -0,0 +1,16 @@
+package connectiondb;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DB {
+	public static Connection getConnection(){
+		Connection con=null;
+		try{
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", "Baiust", "123");
+		}catch(Exception e){System.out.println(e);}
+		return con;
+	}
+
+}
